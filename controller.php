a00030cf974eb595e1385f0587d5b298cc8bcb6c
a3020 <a3020@fastmail.com>
2017-08-06 10:15:28 +0200

    Decouple more from Slife
    - Make pkg controller less dependent on Slife.
    - Check if Slife is installed.
    - Changelog / version bump.

--- controller.php
+++ controller.php
@@ -3,13 +3,14 @@
 namespace Concrete\Package\SlifeCommunityStore;
 
 use Concrete\Core\Support\Facade\Events;
-use Slife\Integration\SlifePackageController;
+use Concrete\Core\Package\Package as BasePackage;
+use Concrete\Core\Support\Facade\Package;
 
-class Controller extends SlifePackageController
+class Controller extends BasePackage
 {
     protected $pkgHandle = 'slife_community_store';
     protected $appVersionRequired = '8.2';
-    protected $pkgVersion = '0.9.0';
+    protected $pkgVersion = '0.9.1';
     protected $pkgAutoloaderRegistries = [
         'src' => '\SlifeCommunityStore',
     ];
@@ -31,6 +32,10 @@ class Controller extends SlifePackageController
 
     public function on_start()
     {
+        if (!$this->isSlifeInstalled()) {
+            return;
+        }
+
         $th = $this->app->make('helper/text');
 
         // Register event listeners
@@ -40,6 +45,45 @@ class Controller extends SlifePackageController
                 'package' => $this->getPackageEntity(),
             ]);
             Events::addListener($eventHandle, [$listener, 'run']);
+        }
+    }
+
+    public function validate_install($data = [])
+    {
+        $error = $this->app->make('error');
+
+        if (!$this->isSlifeInstalled()) {
+            $error->add(
+                t(
+                    "Installation requires <a href='%s' target='_blank'>Slife</a> to be installed.",
+                    "https://www.concrete5.org/marketplace/addons/slife/"
+                )
+            );
+        }
+
+        return $error;
+    }
+
+    /**
+     * @return bool
+     */
+    protected function isSlifeInstalled()
+    {
+        $basePackage = Package::getByHandle('slife');
+        return is_object($basePackage);
+    }
+
+    protected function installEvents()
+    {
+        $th = $this->app->make('helper/text');
+
+        foreach ($this->supportedEvents as $eventHandle) {
+            $className = $th->camelcase($eventHandle);
+            $eventClass = $this->app->make('SlifeC5Events\Event\\'.$className, [
+                'package' => $this->getPackageEntity(),
+            ]);
+
+            $eventClass->install();
         }
     }
 }
